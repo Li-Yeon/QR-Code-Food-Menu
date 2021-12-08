9ff3ecbf9c4e84859d15ceb843b66aa93e9ca1f7
iBlank <ngyihwann2000@gmail.com>
2021-12-09 00:43:02 +0800

    qwe

--- php/user.php
+++ php/user.php
@@ -1,18 +1,7 @@
 <?php
 require_once "../db.php";
-
-if(isset($_SESSION['Admin']))
-    {
-
-    }
-    else
-    {
-        echo '<script>alert("You must login first!");</script>';
-        echo '<script>location.href="../admin/login.php";</script>';
-        exit();
-    }  
-
-$currentUser = $_SESSION['currentUser'];
+session_start();
+$currentUser = $_SESSION['Admin'];
 $getCurrentUserQuery = "SELECT Name from users WHERE Username=" . "'" . $currentUser . "'";
 $getCurrentUserSqli = mysqli_query($conn, $getCurrentUserQuery);
 $getCurrentUser = mysqli_fetch_assoc($getCurrentUserSqli);
@@ -20,24 +9,14 @@ $getCurrentUser = $getCurrentUser['Name'];
 
 
 //Delete User
-if (isset($_GET['deleteuser'])){
-    $No = $_GET['deleteuser'];
-    //Check how many users
-    $usercheckSQL = "SELECT COUNT(*) AS Users FROM users;";
-    $usercheckSqli = mysqli_query($conn, $usercheckSQL) or die (mysqli_error($conn));
-    $usercheck = mysqli_fetch_assoc($usercheckSqli);
-    $usercheck = $usercheck['Users'];
-
-    if ($usercheck <= 1) {
-        echo '<script>alert("1 user left! Unable to delete.")</script>';
-    } else 
-    {          
+if (isset($_GET['delete'])){
+    $No = $_GET['delete'];
     $query = "DELETE FROM users WHERE No = '$No'";
     $reset = "ALTER TABLE users DROP No;ALTER TABLE users AUTO_INCREMENT = 1;ALTER TABLE users ADD No int UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST;";
     $resultDelete = mysqli_query($conn, $query) or die (mysqli_error($conn));
     $resetIncrement = mysqli_multi_query($conn, $reset);
-    echo '<script>location.href="./users.php"</script>';
-    }
-
+    echo '<script>location.href="../users.php"</script>';
 }  
+
+
 ?>
