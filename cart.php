3d28770acf59d9c10c8159bfce74de838c4fe29f
Ijack Jr <ijackjunior77@gmail.com>
2021-12-08 16:34:54 +0800

    cart

--- cart.php
+++ cart.php
@@ -1,12 +1,60 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title> Shopping Cart</title>
+    <link rel="stylesheet" type="text/css" href="cart.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.cssS">
 </head>
 <body>
     
+<div class="container">
+    <h1>Shopping Cart</h1>
+    <div class="cart">
+        <div class="products">
+            <div class="product">
+                <img src="images/nasi_goreng.jpg" alt="">
+                <div class="product-info">
+                        <h3 class="product-name">Nasi Goreng</h3>
+                        <h2 class="product-price">RM 7.50</h2>
+                        <h2 class="product-offer">50%</h2>
+                        <p class="product-quantity">Qnt: <input value="1"name="">
+                        <p class="product-remove">
+                           <i class="fa fa-times" aria-hidden="true"></i>
+                           <span class="remove">Remove</span>
+                        </p>
+                    </div>
+            </div>
+            <div class="product">
+                <img src="images/nasi_goreng_s.jpg" alt="">
+                <div class="product-info">
+                        <h3 class="product-name">Nasi Goreng Seafood</h3>
+                        <h2 class="product-price">RM 7.50</h2>
+                        <h2 class="product-offer">50%</h2>
+                        <p class="product-quantity">Qnt: <input value="1"name="">
+                        <p class="product-remove">
+                           <i class="fa fa-times" aria-hidden="true"></i>
+                           <span class="remove">Remove</span>
+                        </p>
+                    </div>
+            </div>
+        </div>
+        <div class="cart-total">
+            <p>
+                <span>Total Price</span>
+                <span>RM 15.00</span>
+            </p>
+            <p>
+                <span>Number of Items</span>
+                <span>2</span>
+            </p>
+            <p>
+                <span>You Save</span>
+                <span>RM 5.00</span>
+            </p>
+            <a href="#">Proceed to Checkout</a>
+        </div>
+    </div>
+</div>
+
 </body>
 </html>
